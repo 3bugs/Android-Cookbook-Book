6730928edd7c7a2584ef9b4c213a97554e1ca444
promlert <promlert@gmail.com>
2014-07-31 15:25:57 +0700

    first commit

---
+++ SwipeViewsDemo/src/com/example/swipeviewsdemo/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.swipeviewsdemo;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends Activity {
+
+    // ชื่อดาวเคราะห์ทั้งหมดในระบบสุริยะ
+    protected static final String[] mPlanetTitles = new String[] { "Mercury",
+            "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+
+    PlanetPagerAdapter mAdapter; // pager adapter
+    ViewPager mViewPager;
+
+    // ออบเจ็ค OnPageChangeListener สำหรับระบุโค้ดการทำงานเมื่อมีการเปลี่ยนเพจใน ViewPager
+    ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageSelected(int position) {
+            setTitle(mAdapter.getPageTitle(position));
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset,
+                int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+         * สร้างออบเจ็คที่เป็น pager adapter (อินสแทนซ์ของ PlanetPagerAdapter)
+         * แล้วนำไปกำหนดให้กับ ViewPager
+         */
+        mAdapter = new PlanetPagerAdapter(getFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAdapter);
+
+        // กำหนดออบเจ็ค OnPageChangeListener ให้กับ ViewPager
+        mViewPager.setOnPageChangeListener(mPageChangeListener);
+        // แสดงชื่อดาวเคราะห์แรกบน title bar ในตอนเริ่มต้น
+        mPageChangeListener.onPageSelected(0);
+    }
+
+    public class PlanetPagerAdapter extends FragmentStatePagerAdapter {
+        public PlanetPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            // สร้างแฟรกเมนต์ที่แสดงภาพดาวเคราะห์ แล้วส่งคืนแฟรกเมนต์นี้กลับออกไป
+            String planetTitle = mPlanetTitles[i];
+            Fragment fragment = PlanetDetailsFragment.newInstance(planetTitle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return mPlanetTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mPlanetTitles[position];
+        }
+    }
+}
---
+++ SwipeViewsDemo/src/com/example/swipeviewsdemo/PlanetDetailsFragment.java
@@ -0,0 +1,54 @@
+package com.example.swipeviewsdemo;
+
+import java.util.Locale;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class PlanetDetailsFragment extends Fragment {
+
+    // คีย์ของข้อมูล (ชื่อดาวเคราะห์) ที่จะเก็บไว้ที่ตัวแฟรกเมนต์เอง
+    private static final String ARG_PLANET_TITLE = "planet_title";
+    // ชื่อดาวเคราะห์ที่จะแสดงภาพออกมาในแฟรกเมนต์นี้
+    private String mPlanetTitle;
+
+    /*
+     * Factory method ที่ใช้สร้างอินสแทนซ์ของแฟรกเมนต์ แทนการใช้คอนสตรัคเตอร์
+     * โดยจะกำหนดชื่อดาวเคราะห์เป็นอาร์กิวเมนต์ติดไปกับแฟรกเมนต์ด้วย
+     */
+    public static PlanetDetailsFragment newInstance(String planetTitle) {
+        PlanetDetailsFragment fragment = new PlanetDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PLANET_TITLE, planetTitle);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mPlanetTitle = getArguments().getString(ARG_PLANET_TITLE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_planet_details,
+                container, false);
+        ImageView planetImage = (ImageView) layout
+                .findViewById(R.id.planet_image);
+
+        int imageId = getResources().getIdentifier(
+                mPlanetTitle.toLowerCase(Locale.getDefault()), "drawable",
+                getActivity().getPackageName());
+        planetImage.setImageResource(imageId);
+
+        return layout;
+    }
+}
